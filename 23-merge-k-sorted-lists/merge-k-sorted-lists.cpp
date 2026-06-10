bac19b3e1119369c4f484e04b921f1581945a03a
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-06-10 14:24:06 +0530

    Time: 3 ms (61.95%) | Memory: 18.5 MB (44.59%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,24 +1,40 @@
+class Compare {
+public:
+    bool operator()(ListNode* a, ListNode* b) {
+        return a->val > b->val;
+    }
+};
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> allValues;
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
 
-        for(auto list : lists){
-            while(list != NULL){
-                allValues.push_back(list->val);
-                list = list->next;
-            }
+        // Push the head of each non-empty list into the heap
+        for (auto list : lists) {
+            if (list != NULL)
+                pq.push(list);
         }
-        sort(allValues.begin(), allValues.end());
 
+        // Create a dummy node to build the result list
         ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+        ListNode* tail = dummy;
 
-        for (int val : allValues) {
-            curr->next = new ListNode(val);
-            curr = curr->next;
+        // While the heap is not empty
+        while (!pq.empty()) {
+            // Extract the node with the smallest value
+            ListNode* smallest = pq.top();
+            pq.pop();
+
+            // Add it to the result list
+            tail->next = smallest;
+            tail = tail->next;
+
+            // If there's a next node, push it into the heap
+            if (smallest->next != NULL)
+                pq.push(smallest->next);
         }
 
-        return dummy->next;        
+        // Return the head of the merged list
+        return dummy->next;
     }
 };
