e54f63fa7ae07da91a5418477321ecc124cefe0a
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-06-12 17:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (18.35%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void ReverseArray(vector<int>& nums, int start, int end){
+        while(start < end){
+            swap(nums[start],nums[end]);
+            start++;
+            end--;
+        }
+    }
+    vector<int> rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k == 0 || n == 0) return nums;
+        k = k%n;
+        ReverseArray(nums,0,n-1);
+        ReverseArray(nums,0,k-1);
+        ReverseArray(nums,k,n-1);
+        return nums;
+    }
+};
