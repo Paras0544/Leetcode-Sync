67e03682319d83373b420654d2a3871459792c13
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-06-24 16:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (83.63%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,20 +2,34 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candies(n,1);
+        int candies = n;
 
-        for(int i = 1; i<n; i++){
-            if(ratings[i] > ratings[i-1] && candies[i] <= candies[i-1]){
-                candies[i] = candies[i-1] + 1;
-        }
-        }
+        int i = 1;
 
-        for(int i = n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]){
-                candies[i] = candies[i+1] + 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
             }
-        }
 
-        return accumulate(candies.begin(), candies.end(), 0);        
+            int peak = 0;
+
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                candies += peak;
+                i++;
+            }
+
+            int valley = 0;
+
+            while(i<n && ratings[i] < ratings[i-1]){
+                valley++;
+                candies += valley;
+                i++;
+            }
+            candies -= min(peak,valley);
+        }
+        return candies ;
+        
     }
 };
