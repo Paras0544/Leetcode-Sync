e73202ba4fa534f1de24419a318850cb45cdf366
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-06-30 15:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (66.17%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,27 +1,49 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& arr){
-        if(root == NULL){
-            return;
+// Function to perform inorder traversal
+// of a binary tree iteratively
+vector<int> inorderTraversal(TreeNode* root){
+    // Initialize a stack to track nodes
+    stack<TreeNode*> st;
+    // Start from the root node
+    TreeNode* node = root;
+    // Initialize a vector to store
+    // inorder traversal result
+    vector<int> inorder;
+
+    // Start an infinite
+    // loop for traversal
+    while(true){
+        // If the current node is not NULL
+        if(node != NULL){
+            // Push the current
+            // node to the stack
+            st.push(node);
+            // Move to the left child
+            // of the current node
+            node = node->left;
         }
-        inorder(root->left,arr);
-        arr.push_back(root->val);
-        inorder(root->right,arr);
+        else{
+            // If the stack is empty,
+            // break the loop
+            if(st.empty()){
+                break;
+            }
+            // Retrieve a node from the stack
+            node = st.top();
+            // Remove the node from the stack
+            st.pop();
+            // Add the node's value to
+            // the inorder traversal list
+            inorder.push_back(node->val);
+            // Move to the right child
+            // of the current node
+            node = node->right;
+        }
     }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> arr;
-        inorder(root,arr);
-        return arr;
-    }
+    // Return the inorder
+    // traversal result
+    return inorder;
+}
+
 };
