98d995a45e80f8e7c4fa4196ea840ac165f806d7
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-07-23 16:10:02 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (52.43%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-
     TreeNode* findMin(TreeNode* root){
-        while(root->left)
+        while(root->left){
             root = root->left;
-
+        }
         return root;
     }
-
     TreeNode* deleteNode(TreeNode* root, int key) {
-
-        if(root == NULL)
+        if(root == NULL){
             return NULL;
+        }
 
         if(key < root->val){
             root->left = deleteNode(root->left, key);
         }
+
         else if(key > root->val){
             root->right = deleteNode(root->right, key);
         }
         else{
-
-            // No left child
+            //No Left Child
             if(root->left == NULL){
-                TreeNode* temp = root->right;
-                delete root;
-                return temp;
+                return root->right;
             }
-
-            // No right child
+            //No Right Child
             if(root->right == NULL){
-                TreeNode* temp = root->left;
-                delete root;
-                return temp;
+                return root->left;
             }
-
-            // Two children
+            else{
+                //two Children
             TreeNode* successor = findMin(root->right);
-
             root->val = successor->val;
-
             root->right = deleteNode(root->right, successor->val);
+            }
         }
-
         return root;
     }
 };
